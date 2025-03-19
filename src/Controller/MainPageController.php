9cfd1301d2057fd5144578ef19b8c4c95d1f8f43
jirka8 <jirka.dvorak@gmail.com>
2025-03-19 23:01:04 +0100

    Login and app styling

--- src/Controller/MainPageController.php
+++ src/Controller/MainPageController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class MainPageController extends AbstractController
 {
     #[Route('/', name: 'app_main_page')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
         return $this->render('main_page/index.html.twig', [
